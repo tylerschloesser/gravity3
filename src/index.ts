c91ef85aa9322504848fdf289959a7cf7f0e7a66
Tyler Schloesser <tylerschloesser@gmail.com>
2020-07-25 12:46:07 -0700

    add canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('hi')
+const canvas = document.querySelector('canvas') as HTMLCanvasElement
+const context = canvas.getContext('2d')
+
+context.fillStyle = 'black'
+context.fillRect(0, 0, canvas.width, canvas.height)
